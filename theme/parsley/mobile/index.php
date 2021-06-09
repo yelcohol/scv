5e991b4890a845b73839c8045164dc7fb31b42b8
Cat-oliviaseul <oxok7@naver.com>
2021-06-09 16:17:40 +0900

    화면을 줄였을 때 메뉴가 바로 보이게 하기

--- theme/parsley/mobile/index.php
+++ theme/parsley/mobile/index.php
@@ -11,7 +11,53 @@ add_javascript('<script src="'.G5_JS_URL.'/jquery.bxslider.js"></script>', 10);
 
 <!-- 메인 최신글 시작 -->
 <div class="conle_idx_top">
-	
+<?php echo outlogin("theme/basic_side"); ?>
+    <div id="gnb_index">
+        <div class="gnb_side">
+			<h2>메인메뉴</h2>
+			<ul class="gnb_1dul">
+            <?php
+            $sql = " select *
+                        from {$g5['menu_table']}
+                        where me_mobile_use = '1'
+                          and length(me_code) = '2'
+                        order by me_order, me_id ";
+            $result = sql_query($sql, false);
+
+            for($i=0; $row=sql_fetch_array($result); $i++) {
+            ?>
+                <li class="gnb_1dli">
+                    <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_1da"><i class="far fa-list-alt"></i> <?php echo $row['me_name'] ?></a>
+                    <?php
+                    $sql2 = " select *
+                                from {$g5['menu_table']}
+                                where me_mobile_use = '1'
+                                  and length(me_code) = '4'
+                                  and substring(me_code, 1, 2) = '{$row['me_code']}'
+                                order by me_order, me_id ";
+                    $result2 = sql_query($sql2);
+
+                    for ($k=0; $row2=sql_fetch_array($result2); $k++) {
+                        if($k == 0)
+                            echo '<button class="btn_gnb_op">하위분류</button><ul class="gnb_2dul">'.PHP_EOL;
+                    ?>
+                        <li class="gnb_2dli"><a href="<?php echo $row2['me_link']; ?>" target="_<?php echo $row2['me_target']; ?>" class="gnb_2da"><?php echo $row2['me_name'] ?></a></li>
+                    <?php
+                    }
+
+                    if($k > 0)
+                        echo '</ul>'.PHP_EOL;
+                    ?>
+                </li>
+            <?php
+            }
+
+            if ($i == 0) {  ?>
+                <li id="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <br><a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하세요.<?php } ?></li>
+            <?php } ?>
+            </ul>
+        </div>
+	</div>
 	<div class="conle_lt">
 	<?php
     // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
