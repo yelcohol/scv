56e0c29c8acde17a0dffd595b3f7c2e8be5e5dc2
jwMah <wigbuldok@naver.com>
2021-06-29 11:01:28 +0900

    리스트 지원하기 버튼 조건 수정

--- scv/theme/parsley/mobile/skin/board/work/list.skin.php
+++ scv/theme/parsley/mobile/skin/board/work/list.skin.php
@@ -170,16 +170,16 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
 							<?php $bbs=G5_BBS_URL?>
 							<?php 
 							if($is_worker){
-								if($list[$i]['ca_name']!='모집종료' && $list[$i]['ca_name'] != '모집취소'){
+								if($list[$i]['ca_name']=='모집종료' || $list[$i]['ca_name'] == '모집취소'){
+									echo '<h3 style="color:red"><b>지원마감</b></h3>';
+								}
+								else{
 									if($list[$i]['ca_name'] == '재모집'){
 										echo '<a href="./apply_memo_form.php?re=1'.'&me_recv_mb_id='.$list[$i]['mb_id'].'&bo_table='.$bo_table.'&wr_id='.$list[$i]['wr_id'].'" target="_blank" class="btn btn_b03" onclick="win_apply(this.href); return false;"><i class="fa fa-check-circle"></i> <span class="hidden-xs">지원하기</span></a>';	
 									}else{
 										//echo '<a href="./apply_popin.php?bo_table='.$bo_table.'&wr_id='.$list[$i]['wr_id'].' target="_blank" class="btn btn_b03" onclick="win_apply(this.href); return false;"><i class="fa fa-check-circle"></i> <span class="hidden-xs">지원하기</span></a>';
 										echo '<a href="./apply_memo_form.php?'.'me_recv_mb_id='.$list[$i]['mb_id'].'&bo_table='.$bo_table.'&wr_id='.$list[$i]['wr_id'].'" target="_blank" class="btn btn_b03" onclick="win_apply(this.href); return false;"><i class="fa fa-check-circle"></i> <span class="hidden-xs">지원하기</span></a>';
 									}
-								}
-								else{
-									echo '<h3 style="color:red"><b>지원마감</b></h3>';
 								}
 							}?>
 							<?php if($is_constructor && $member['mb_id'] == $list[$i]['mb_id']) { ?>
